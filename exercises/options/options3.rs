bbed068b985df5fe5a1c501dcf7b1666af399940
Kunwu Chan <kunwu.chan@hotmail.com>
2025-02-26 17:14:41 +0800

    20250226 part 1

--- exercises/options/options3.rs
+++ exercises/options/options3.rs
@@ -3,7 +3,6 @@
 // Execute `rustlings hint options3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 struct Point {
     x: i32,
@@ -12,10 +11,13 @@ struct Point {
 
 fn main() {
     let y: Option<Point> = Some(Point { x: 100, y: 200 });
-
-    match y {
-        Some(p) => println!("Co-ordinates are {},{} ", p.x, p.y),
-        _ => panic!("no match!"),
-    }
+    // if let默认移动值，除非你用ref借用
+    if let Some(ref p) = y{ //将p作为引用绑定到y内的Point，而不是Move
+        println!("Co-ordinates are {},{} ", p.x, p.y);
+    };
+    // match y {
+    //     Some(p) => println!("Co-ordinates are {},{} ", p.x, p.y),
+    //     _ => panic!("no match!"),
+    // }
     y; // Fix without deleting this line.
 }
